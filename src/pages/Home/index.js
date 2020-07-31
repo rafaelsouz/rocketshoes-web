df183a4bd26d274e9a43ddb07840b7b45fed6244
Rafael Souza <rafael.souza@umbu.tech>
2020-07-31 11:38:56 -0300

    Chore: Estilizando a pagina Home da aplicação

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,98 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Downshifter</strong>
+        <span>199,99</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingBasket size={16} color="#fff" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Downshifter</strong>
+        <span>199,99</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingBasket size={16} color="#fff" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Downshifter</strong>
+        <span>199,99</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingBasket size={16} color="#fff" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Downshifter</strong>
+        <span>199,99</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingBasket size={16} color="#fff" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Downshifter</strong>
+        <span>199,99</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingBasket size={16} color="#fff" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
 
 export default Home;
