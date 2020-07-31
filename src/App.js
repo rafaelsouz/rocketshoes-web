9d570881a8329827e632093a5be7787b79616cb0
Rafael Souza <rafael.souza@umbu.tech>
2020-07-31 09:48:58 -0300

    Chore: Implementado a estilização global e as rotas de carrinho e home.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Oi mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
