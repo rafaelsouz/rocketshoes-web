fe4109cd478dfba1eb907c2b492077618a8f4e0b
Rafael Souza <rafael.souza@umbu.tech>
2020-07-31 21:45:59 -0300

    Chore: API configurada e listando produtos da api no home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,96 +1,35 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { MdShoppingBasket } from 'react-icons/md';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('/products').then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Downshifter</strong>
-        <span>199,99</span>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.price}</span>
 
-        <button type="button">
-          <div>
-            <MdShoppingBasket size={16} color="#fff" />3
-          </div>
+          <button type="button">
+            <div>
+              <MdShoppingBasket size={16} color="#fff" />3
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Downshifter</strong>
-        <span>199,99</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingBasket size={16} color="#fff" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Downshifter</strong>
-        <span>199,99</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingBasket size={16} color="#fff" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Downshifter</strong>
-        <span>199,99</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingBasket size={16} color="#fff" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-nike-downshifter-10-masculino/02/HZM-3549-002/HZM-3549-002_zoom1.jpg?ts=1584657883&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Downshifter</strong>
-        <span>199,99</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingBasket size={16} color="#fff" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }
